10ce30e059e793562759a6db6ae267d0125d993f
fishedee <306766045@qq.com>
2014-12-01 21:52:29 +0800

    加入crontab部署

--- util/install.sh
+++ util/install.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-sudo apt-get install imagemagick
+sudo apt-get install imagemagick -y
+sudo crontab -l > /tmp/crontab.bak
+sudo echo "50 * * * * /home/fish/Project/weixiu/data/mysql/backup.sh > /dev/null 2>&1" >> /tmp/crontab.bak
+sudo crontab /tmp/crontab.bak
+sudo crontab -l
