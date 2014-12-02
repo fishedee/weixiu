5edb4a7c2d43b753d52b02dd5399b3c7ef2cff5e
fishedee <306766045@qq.com>
2014-12-02 15:10:16 +0800

    修复分享出去的链接问题

---
+++ util/makejs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ADMIN_OUT=../static/src/card/
+for i in `ls ${ADMIN_OUT}`; do \
+	for j in `ls ${ADMIN_OUT}/${i}`; do \
+		#更改所有js的位置
+		if [ "${j}" != "icon.jpg" -a "${j}" != "index.html" ];then \
+			rm ${ADMIN_OUT}/${i}/${j}/new_wsmain.js;\
+			ln -s ../../../js/new_wsmain.js ${ADMIN_OUT}/${i}/${j}/new_wsmain.js;\
+		fi \
+	done \
+done
